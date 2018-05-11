8024d1f45ce4376a756bf368bc16a3307a030d33
Julian Furchert <work@julianfurchert.de>
2018-05-11 17:06:51 +0200

    update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import ReactSlider from 'react-slider';
 
 import './Slider.css'
 
 export default class Slider {
   render(){
     return(
-      <ReactSlider defaultValue={[0, 100]} withBars />
+      <div/>
     );
   }
 }
